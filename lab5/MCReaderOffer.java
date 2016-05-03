009f60f2ad2b613d80a3dc4064c9757f4a99d1dc
hannhuan <hannhuan@hotmail.com>
2016-05-03 07:37:59 +0200

    Added files via upload

--- lab5/MCReaderOffer.java
+++ lab5/MCReaderOffer.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MCReaderOffer {
 
@@ -15,20 +18,45 @@ public class MCReaderOffer {
 				MulticastSocket ms = new MulticastSocket(4099);
 				InetAddress ia = InetAddress.getByName("experiment.mcast.net");
 				ms.joinGroup(ia);
+				
+				OfferThread thread = new OfferThread();
+				thread.start();
+				
 				while (true) {
 					byte[] buf = new byte[65536];
 					DatagramPacket dp = new DatagramPacket(buf, buf.length);
 					ms.receive(dp);
 					
 					
-					String s = new String(dp.getData(), 0, dp.getLength());
+					
+					String s = new String(dp.getData(), 0, dp.getLength(), "UTF-8");
 					System.out.println("Received: " + s);
 					
-
-					InetAddress host =  InetAddress.getLocalHost();
-					byte[] hostInfo = host.toString().getBytes();
-					DatagramPacket hostInfoP = new DatagramPacket (hostInfo, hostInfo.length);
-					ms.send(hostInfoP);
+					DateFormat df = null;
+					
+					switch (s){
+					case "Address":
+						InetAddress host =  InetAddress.getLocalHost();
+						byte[] hostInfo = host.toString().getBytes();
+						DatagramPacket hostInfoP = new DatagramPacket (hostInfo, hostInfo.length, dp.getAddress(), dp.getPort());
+						ms.send(hostInfoP);
+					case "date":
+						df = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH);
+						break;
+					case "time":
+						df = DateFormat.getTimeInstance(DateFormat.FULL, Locale.ENGLISH);
+						break;
+					default:
+						System.out.println("We don't understand your command.");
+						return;	
+					}
+					
+					if (df != null){
+					String time = df.format(new Date());
+					ms.send(new DatagramPacket(time.getBytes(), time.getBytes().length, dp.getAddress(), dp.getPort()));
+					
+					}
+					
 				}
 			} catch (IOException e) {
 				System.out.println("Exception:" + e);
---
+++ lab5/OfferThread.java
@@ -0,0 +1,20 @@
+package lab5;
+
+import java.net.DatagramSocket;
+
+public class OfferThread extends Thread{
+	
+	public OfferThread(){
+		try{
+			DatagramSocket socket = new DatagramSocket();
+		} catch (Exception e){
+			
+		}
+		
+	}
+	
+	public void run(){
+		
+	}
+
+}
